22fc2d4e5f5f60f2fa82af4783618b880bae47b1
dfguan <dfguan@hit.edu.cn>
2018-05-23 10:53:39 +0100

    complete alignment part

--- 180522.align.bash
+++ 180522.align.bash
@@ -2,20 +2,22 @@
 # Contact: Dengfeng Guan, dfguan@hit.edu.cn
 # Purpose: GATK Variant calling for mulitple samples using fAstCal as reference
 # Date: 180522
-
+set -x
 align() {
 	fn=$1
 	idxbase=$2
 	pre_fn=$(basename $fn .bam)
-	samtools fastq -1 "$pre_fn"_1.fq -2 "$pref_fn"_2.fq $fn
-	# align and sort bam
-	bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -K1 -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pref_fn}_1.fq ${pref_fn}_2.fq | samtools sort -O BAM -o ${pref_fn}.srt.bam &" 
-	# samtools remove duplicates
-	samtools rmdup ${pref_fn}.srt.bam > ${pref_fn}.srt.rmdup.bam
-	rm -f ${pref_fn}.srt.bam
+	cd $(dirname $fn)
+	samtools fastq -1 "$pre_fn"_1.fq -2 "$pre_fn"_2.fq $fn
+	echo "align and sort"
+	# align & sort bam & rm duplicates
+	#bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -K -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pre_fn}_1.fq ${pre_fn}_2.fq | samtools sort -O BAM | samtools rmdup - ${pre_fn}.srt.rmdup.bam && rm -f ${pre_fn}_[12].fq"  
+	bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pre_fn}_1.fq ${pre_fn}_2.fq | samtools sort -O BAM | samtools rmdup - ${pre_fn}.srt.rmdup.bam && rm -f ${pre_fn}_[12].fq"  
 	# gatk recalibrate or maybe group bams first what I am concerned is the fish genomes can be different	
 }
-ref=$1
-seq_dir=$2
+
+ref=$(readlink -f $1)
+seq_dir=$(readlink -f $2)
 bwa index $ref  
-find seq_dir -name "*.bam" | xargs -n 1 -P8 -i sh -c "align {} $ref" 
+export -f align
+find $seq_dir -name "*.bam" | xargs -n 1 -P8 -i bash -c "align {} $ref &" 
