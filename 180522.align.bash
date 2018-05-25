c933ac809f2c89f9f2d468bff6eaf006e660012d
dfguan <dfguan@hit.edu.cn>
2018-05-25 17:02:29 +0100

    use combinegvcfs

--- 180522.align.bash
+++ 180522.align.bash
@@ -12,12 +12,13 @@ align() {
 	echo "align and sort"
 	# align & sort bam & rm duplicates
 	#bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -K -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pre_fn}_1.fq ${pre_fn}_2.fq | samtools sort -O BAM | samtools rmdup - ${pre_fn}.srt.rmdup.bam && rm -f ${pre_fn}_[12].fq"  
-	bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pre_fn}_1.fq ${pre_fn}_2.fq | samtools sort -O BAM | samtools rmdup - ${pre_fn}.srt.rmdup.bam && rm -f ${pre_fn}_[12].fq"  
+	bsub -M4000 -n4 -R"span[hosts=1] select[mem>4000] rusage[mem=4000]" -Jalign_"$pre_fn" -oalign_"$pre_fn".o "bwa mem -t4 ${idxbase} ${pre_fn}_1.fq ${pre_fn}_2.fq | samtools sort -O BAM -o ${pre_fn}.srt.bam && rm -f ${pre_fn}_[12].fq" & 
 	# gatk recalibrate or maybe group bams first what I am concerned is the fish genomes can be different	
 }
 
 ref=$(readlink -f $1)
 seq_dir=$(readlink -f $2)
-bwa index $ref  
+#bwa index $ref  
 export -f align
-find $seq_dir -name "*.bam" | xargs -n 1 -P8 -i bash -c "align {} $ref &" 
+find $seq_dir -name "*.srt.bam" -o -name "*.srt.rmdup.bam" -exec rm {} \;
+find $seq_dir -name "*.bam" | xargs -n 1 -P8 -i bash -c "align {} $ref" 
--- 180524.gatk_vc.bash
+++ 180524.gatk_vc.bash
@@ -1,24 +1,39 @@
 #!/bin/bash
-# Contact: Dengfeng Guan, dfguan@hit.edu.cn
+# Contact: Dengfeng Guan, dg30@sanger.ac.uk, dfguan@hit.edu.cn
 # Purpose: GATK Variant calling for mulitple samples using fAstCal as reference
 # Date: 180524
 
-set -x 
+
+md() {
+	cur_dir=$(pwd)
+	srt_bam=$1
+	pre_fn=$(basename $srt_bam .srt.bam)
+	ref=$2
+	picard_path=$3
+	cd $(dirname $srt_bam)
+	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jvc_"$pre_fn" -ovc_"$pre_fn".o -K -evc_"$pre_fn".e "java -jar -Xmx4G  $picard_path MarkDuplicates I=${srt_bam} O=${pre_fn}.srt.rmdup.bam M=${pre_fn}.rmdup_matrix.txt AS=true" & 
+	cd $cur_dir
+}
 vc() {
+	cur_dir=$(pwd)
 	rmdup_srt_bam=$1
-	pre_fn=$(basename $rmdup_srt_bam .srt.bam)
+	pre_fn=$(basename $rmdup_srt_bam .srt.rmdump.bam)
 	ref=$2
 	gatk_path=$3
-	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jvc_"$pre_fn" -ovc_"$pre_fn".o -K -evc_"$pre_fn".e "java -jar -Xmx4G  ${gatk_path} HaplotypeCaller -R ${ref}  -I ${rmdup_srt_bam} -ERC GVCF -o ${pre_fn}.raw.g.vcf"  
+	cd $(dirname $rmdup_srt_bam)
+	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jvc_"$pre_fn" -ovc_"$pre_fn".o -K -evc_"$pre_fn".e "java -jar -Xmx4G  ${gatk_path} HaplotypeCaller -R ${ref}  -I ${rmdup_srt_bam} -ERC GVCF -G Standard -GAS_Standard -O ${pre_fn}.raw.g.vcf" & 
+	cd $cur_dir
+	#sleep 100
 }
+
 #consolidate variations
 cv() {
 	gatk_path=$1
 	gvcfs=$2
 	ref=$3
 	out=$4
-	java -jar -Xmx4G $gatk_path GenomicsDBImport $gvcfs --genomicsdb-workspace-path gvcf_db
-	java -jar -Xmx4G $gatk_path GenotypeGVCFs -R $ref -V gendb://gvcf_db -G StandardAnnotation -newQual -O $out 
+	#java -jar -Xmx4G $gatk_path GenomicsDBImport $gvcfs --genomicsdb-workspace-path gvcf_db
+	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jcv_gvcf -ocv_gvcf.o -K -ecv_gvcf.e "java -jar -Xmx4G $gatk_path CombineGVCFs $gvcfs -R $ref -O massoko.cmb.g.vcf && java -jar -Xmx4G $gatk_path GenotypeGVCFs -R $ref -V massoko.cmb.g.vcf -O $out" &
 }
 
 #filter variations
@@ -27,36 +42,45 @@ fltv() {
 	raw_vcf=$2
 	ref=$3
 	pref=$(basename $raw_vcf .vcf)
-	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jfltv_"$pre_fn" -ofltv_"$pre_fn".o -K -efltv_"$pre_fn".e "java -jar -Xmx4G ${gatk_path} VariantFiltration -R $ref -V $raw_vcf --filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filterName "basic_filtering" -o ${pref}_flted.vcf"
- 
+	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jfltv_"$pre_fn" -ofltv_"$pre_fn".o -efltv_"$pre_fn".e "java -jar -Xmx4G ${gatk_path} VariantFiltration -R $ref -V $raw_vcf -filter "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "basic_filtering" -O ${pref}_flted.vcf"
 }
 
 
-
 export -f vc
+export -f md
 export -f cv
 export -f fltv
 
-ref=$(readlink -f $1)
-bam_dir=$(readlink -f $2)
-gatk_path=$(readlink -f $3)
-
-echo "Run Haplotype Variant Calling"
-find $bam_dir -name "*.srt.bam" | xargs -n 1 -P8 -i bash -c "vc {} $ref $gatk_path &" 
-wait 
-
-echo "Consolidating Variations..."
-gvcf_dir=$bam_dir
-gvcfs=""
-find $gvcf_dir -name "*.g.vcf" | xargs -n1 -i gvcfs=$gvcfs"-v {}"
-if [ $gvcfs = "" ]
+if [ "$#" -lt 4 ]
 then
-	echo "No GVCF Files found"
+	echo gatk_vc REFERENCE BAM_DIR GATK_PATH PICARD_PATH
 else
-	out=massoko.vcf
-  	bsub -M4000 -R"select[mem>4000] rusage[mem=4000]" -Jvc_"$pre_fn" -ovc_"$pre_fn".o -K -evc_"$pre_fn".e -I cv $gatk_path $gvcfs $ref $out &
+	ref=$(readlink -f $1)
+	bam_dir=$(readlink -f $2)
+	gatk_path=$(readlink -f $3)
+	picard_path=$(readlink -f $4)
+	pre_ref=$(basename $ref .fa)
+	echo "Create Sequence Dictionary"
+	#samtools dict $ref -o "$pre_ref".dict
+	echo "Marking Duplicates..."
+	find $bam_dir -name "*.srt.bam" | xargs -n 1 -P8 -i bash -c "md {} $ref $picard_path" 
 	wait
-	echo "Filtering Variations..."
-	fltv $gatk $out $ref 
+	echo "Run Haplotype Variant Calling"
+	find $bam_dir -name "*.srt.rmdup.bam" | xargs -n 1 -P8 -i bash -c "vc {} $ref $gatk_path" 
+	#find $bam_dir -name "*.srt.rmdup.bam" | xargs -n 1 -P8 -i bash -c "vc {} $ref $gatk_path" 
+	wait
+	echo "Consolidating Variations..."
+	gvcf_dir=$bam_dir
+	gvcfs=""
+	find $gvcf_dir -name "*.g.vcf" | xargs -n1 -i gvcfs=$gvcfs"-V {} "
+	if [ "$gvcfs" = "" ]
+	then
+		echo "No GVCF Files found"
+	else
+		out=massoko.vcf
+		cv $gatk_path $gvcfs $ref $out 
+		wait
+		echo "Filtering Variations..."
+		fltv $gatk $out $ref 
+	fi
 fi
-
